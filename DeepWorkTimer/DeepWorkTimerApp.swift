766ee50675e5723125732eed04043d818c71c34b
Andrey Tarantsov <andrey@tarantsov.com>
2022-05-09 02:48:49 +0400

    Initial Commit

---
+++ DeepWorkTimer/DeepWorkTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepWorkTimerApp.swift
+//  DeepWorkTimer
+//
+//  Created by Andrey Tarantsov on 2022-05-09.
+//
+
+import SwiftUI
+
+@main
+struct DeepWorkTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
